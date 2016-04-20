4833da9c4af9a71ce0987fce4bb254b6f5e773ad
zhuravlik <2rs.kei@gmail.com>
2016-04-20 15:47:22 +0900

    SQL関係整備、動作確認済

--- fc-calc.php
+++ fc-calc.php
@@ -10,7 +10,7 @@ if ($mysqli->connect_error) {
     $mysqli->set_charset("utf-8");
 }
 
-$userid = 1;
+$uid = 1;
 $dist = isset($argv[1])? (double)$argv[1]: null;
 $fuel = isset($argv[2])? (double)$argv[2]: null;
 
@@ -19,47 +19,48 @@ if(($dist <= 0) or ($fuel <= 0)){
   exit;
 }
 
-$recordSet = $mysqli->query("SELECT * FROM fuelcost_save WHERE id = $userid");
-$data = mysqli_use_result($mysqli);
+$sql = "SELECT * FROM fuelcost_save WHERE userid = $uid";
+$result = $mysqli->query($sql);
+$data = $result->fetch_assoc();
 
 
   if($dist <= $data['dist_total']){
-    $totaldist = $dist + $data['dist_total'];
+    $dist_total = $dist + $data['dist_total'];
     $mode = "TRIP";
   }else{
-    $totaldist = $dist;
+    $dist_total = $dist;
     $dist = $dist - $data['dist_total'];
     $mode = "ODO";
   }
 
-  $fuelcost = $dist / $fuel;
-  $totalfuel = $fuel + $data['fuel_total'];
-  $fuelcost_total = $totaldist / $totalfuel;
+$fuelcost = $dist / $fuel;
+$fuel_total = $fuel + $data['fuel_total'];
+$fuelcost_total = $dist_total / $fuel_total;
 
-  $body = sprintf("[給油記録]：%.1fkm走行し、%.1fL給油しました。燃費%.2fkm/L (前回比:%+.2fkm/L)\n[総走行距離:%.1fkm (前回比:%+.2fkm) 累計燃費:%.2fkm/L (前回比:%+.2fkm/L)]\n[System：%sモードで記録しました。]\n",
+$body = sprintf("[給油記録]：%.1fkm走行し、%.1fL給油しました。燃費%.2fkm/L (前回比:%+.2fkm/L)\n[総走行距離:%.1fkm (前回比:%+.2fkm) 累計燃費:%.2fkm/L (前回比:%+.2fkm/L)]\n[System：%sモードで記録しました。]\n",
                   $dist,
                   $fuel,
                   $fuelcost,
                   $fuelcost - $data['fuelcost_last'],
                   $dist,
-                  $totaldist + $data['dist_add'],
+                  $dist_total + $data['dist_add'],
                   $fuelcost_total,
                   $data['fuelcost_total']-$fuelcost_total,
                   $mode
                  );
 
-  $SQL = sprintf("UPDATE fuelcost_save SET dist_total = $totaldist,
-                                           fuel_total = $totalfuel,
+$SQL = sprintf("UPDATE fuelcost_save SET dist_total = $dist_total,
+                                           fuel_total = $fuel_total,
                                            fuelcost_total= $fuelcost_total,
-                                           fuelcost_last = $fuelcost,
-                                           WHERE id = $userid"
+                                           fuelcost_last = $fuelcost
+                                           WHERE userid = $uid"
                 );
 
-  echo $body;
-  mysqli_query($mysqli,$SQL);
+$mysqli->query($SQL);
 
+echo $body;
 
-  return $body;
+$mysqli->close();
 
 function errmsg($text){
   echo "[Error]{$text}",PHP_EOL;
